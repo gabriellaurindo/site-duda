c084893b85e7206a80f8ef1dac7522c2bdec74e6
batatizando <dudaaabloxx@gmail.com>
2025-11-25 20:53:09 -0300

    monstro

---
+++ jogo.js
@@ -0,0 +1,33 @@
+const monster = document.getElementById("monster");
+const gameArea = document.getElementById("game-area");
+const scoreDisplay = document.getElementById("score");
+
+let score = 0;
+
+// Coloca o monstro em uma posição aleatória
+function moveMonster() {
+  const areaWidth = gameArea.clientWidth;
+  const areaHeight = gameArea.clientHeight;
+
+  const maxX = areaWidth - monster.clientWidth;
+  const maxY = areaHeight - monster.clientHeight;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  monster.style.left = randomX + "px";
+  monster.style.top = randomY + "px";
+}
+
+// Quando clicar no monstro → ganha ponto e ele muda de lugar
+function clicarNoMonstro() {
+  score++;
+  scoreDisplay.textContent = score;
+  moveMonster();
+}
+
+// Faz o monstro se mover sozinho a cada 1 segundo
+setInterval(moveMonster, 3*1000);
+
+// Primeira posição
+moveMonster();
